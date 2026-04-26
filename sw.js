5adc134ae85c23e353d534806db37fe66474bfbc
thetravelatlasapp-create <thetravelatlasapp@gmail.com>
2026-04-26 12:17:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE = 'travelatlas-v1';
+const CACHE = 'travelatlas-v2';
 const ASSETS = ['/'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
